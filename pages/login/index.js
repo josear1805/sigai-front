92d34ee7469781a710f3f68b667579ad1185ee6d
Jose Artigas <jgam310@gmail.com>
2021-09-12 15:45:19 -0400

    se mofica header del proyectoi

--- pages/login/index.js
+++ pages/login/index.js
@@ -19,8 +19,8 @@ const Signin = () => {
     const onFinish = async (values) => {
         const { Usuario, Clave } = values;
         
-        await axios.post(`http://66.23.226.204/indican/validarUsuario.php`, {Usuario, Clave})
         // await axios.post(`http://localhost/indican/validarUsuario.php`, {Usuario, Clave})
+        await axios.post(`http://66.23.226.204/indican/validarUsuario.php`, {Usuario, Clave})
         .then(response => {
             if (response.status === 200) {
                 const { data } = response;
