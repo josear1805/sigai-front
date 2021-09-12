b5ad338577c1e686dfcd12ff5072875168b2a7cd
Jose Artigas <jgam310@gmail.com>
2021-09-12 15:23:05 -0400

    prueba 2

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -12,6 +12,8 @@ const LayoutApp = (props) => {
         <Layout style={{ minHeight: '100vh' }}>
             <Head>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
+
                 {/* <meta httpEquiv="Content-Security-Policy: default-src *://66.23.226.204" content="upgrade-insecure-requests" /> */}
                 {/* <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> */}
                 {/* <meta name="viewport" content="initial-scale=1.0, width=device-width" /> */}
